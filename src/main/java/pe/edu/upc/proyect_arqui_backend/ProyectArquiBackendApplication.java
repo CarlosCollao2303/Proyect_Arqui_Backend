5d07e93f2b274e895fd7deb0221ef48c0dc273c4
Carlos23 <carlos.collao.quiroz@gmail.com>
2026-09-08 00:42:10 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/proyect_arqui_backend/ProyectArquiBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.proyect_arqui_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectArquiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectArquiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/proyect_arqui_backend/ProyectArquiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.proyect_arqui_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectArquiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
